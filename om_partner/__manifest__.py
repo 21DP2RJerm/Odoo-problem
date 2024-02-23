1a978aa8426b86e9ddda55fffc44ba1f19ad69b7
21DP2RJerm <100911489+21DP2RJerm@users.noreply.github.com>
2024-02-23 16:24:54 +0100

    Add files via upload

---
+++ om_partner/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'partner',
+    'version': '1.2',
+    'category': '',
+    'summary': '',
+    'description': """""",
+    'depends': [],
+    'data': ['views/partner.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+}
